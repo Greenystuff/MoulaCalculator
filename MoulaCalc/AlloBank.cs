20320e4c5eb3f8a8aede7dbded2c6dca761fc0e9
Greenystuff <david_meunier@hotmail.fr>
2021-11-06 02:24:31 +0100

    v1.0

--- MoulaCalc/AlloBank.cs
+++ MoulaCalc/AlloBank.cs
@@ -134,6 +134,15 @@ namespace MoulaCalc
             databaseManager.closeDbConnection();
         }
 
+        public void DeleteNbBillets()
+        {
+            string cmd = "DELETE FROM Encours";
+            databaseManager.createDbFile();
+            databaseManager.createDbConnection();
+            databaseManager.executeQuery(cmd);
+            databaseManager.closeDbConnection();
+        }
+
         public void DeleteByRowID(int rowID)
         {
             string cmd = "DELETE FROM AlloBank WHERE Id="+ rowID;
